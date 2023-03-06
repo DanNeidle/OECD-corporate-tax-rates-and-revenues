50dea7bb6fbf45ed51fcccb0dc968d0151474a15
DanNeidle <65954432+DanNeidle@users.noreply.github.com>
2023-03-06 18:18:15 +0000

    Add files via upload

--- corporate_tax_over_time.py
+++ corporate_tax_over_time.py
@@ -11,9 +11,9 @@ import math
 # also requires kaleido package
 
 gdp_data_starting_year = 1965
-gdp_data_end_year = 2018
+gdp_data_end_year = 2019
 rate_data_starting_year = 1965
-rate_data_end_year = 2021
+rate_data_end_year = 2022
 
 # visible_countries = "United Kingdom United States France Germany Denmark Spain Sweden Ireland New Zealand Australia Italy Japan Luxembourg Norway Switzerland Belgium"
 visible_countries = "United Kingdom"
@@ -63,30 +63,23 @@ print("")
 revenue_data = xl.parse("corp-tax-gdp")
 rate_data = xl.parse("rates")
 
-total_revenues = {}
-total_rates = {}
 
+# start with first colour
 line_colour = 0
+
+# loop through countries
 for country_row in range (0,len(revenue_data)):
     country_name = revenue_data.iat[country_row, 0]
-    print (country_name)
+    print(f"Plotting {country_name}:")
 
     # plot revenue data
     x_data = []  # year
     y_data = []  # CT revenues
-    for i in range (0, end_year - gdp_data_starting_year):
+    for i in range (0, end_year - gdp_data_starting_year + 1):
         revenues = revenue_data.iat[country_row, i + 1] / 100
         year = gdp_data_starting_year + i
         x_data.append(year)
         y_data.append(revenues)
-
-        if math.isnan(revenues) or revenues ==0:
-            pass   #  no entry for this year, so keep out of averages
-        else:
-            if year in total_revenues:
-                total_revenues[year].append(revenues)
-            else:
-                total_revenues[year] = [revenues]
 
     # add label to last data item showing country (bit hacky; must be better way)
     labels = [""] * (end_year - gdp_data_starting_year - 2)
@@ -114,19 +107,13 @@ for country_row in range (0,len(revenue_data)):
     # plot rate data
     x_data = []  # year
     y_data = []  # CT rate
-    for i in range(0, end_year - rate_data_starting_year):
+    for i in range(0, end_year - rate_data_starting_year + 1):
         rate = rate_data.iat[country_row, i+1] / 100
         year = rate_data_starting_year + i
         x_data.append(year)
         y_data.append(rate)
-
-        if math.isnan(rate) or rate == 0:
-            pass  # no entry for this year, so keep out of averages
-        else:
-            if year in total_rates:
-                total_rates[year].append(rate)
-            else:
-                total_rates[year] = [rate]
+        if "Kingdom" in country_name:
+            print(f"{year}: {rate}")
 
     # add label to last data item showing country (bit hacky; must be better way)
     labels = [""] * (end_year - rate_data_starting_year - 2)
@@ -141,76 +128,16 @@ for country_row in range (0,len(revenue_data)):
         x=x_data,
         y=y_data,
         mode="lines+text",  # no markers
-        name=country_name,
-        legendgroup=country_name,
+        name=country_name + " rate",
         text=labels,
         textposition="top center",
-        showlegend=False,
+        showlegend=True,
         visible=visibility,
         line=dict(color=colours[line_colour],dash='dash')
     ),
         secondary_y=True)
 
     line_colour += 1
-
-# now compute OECD revenue average
-x_data = []
-y_data = []
-
-for year in total_revenues:
-    x_data.append(year)
-    y_data.append(sum(total_revenues[year]) / len(total_revenues[year]))  # average revenue
-    print(f"{year}: total {sum(total_revenues[year])}, and {len(total_revenues[year])} entries, so average {(sum(total_revenues[year]) / len(total_revenues[year]))}")
-
-print("OECD averages:")
-print(total_revenues[2000])
-
-labels = [""] * (end_year - rate_data_starting_year - 2)
-labels.append("OECD average")
-
-fig.add_trace(go.Scatter(
-    x=x_data,
-    y=y_data,
-    mode="lines+text",  # no markers
-    name='OECD average',
-    legendgroup='OECD average',
-    text=labels,
-    textposition="top center",
-    showlegend=True,
-    visible='legendonly',
-    line=dict(color=colours[line_colour])
-),
-    secondary_y=False)
-
-
-# now compute OECD revenue average
-x_data = []
-y_data = []
-
-for year in total_rates:
-    x_data.append(year)
-    y_data.append(sum(total_rates[year]) / len(total_rates[year]))  # average revenue
-    print(f"{year}: total {sum(total_rates[year])}, and {len(total_rates[year])} entries, so average {(sum(total_rates[year]) / len(total_rates[year]))}")
-
-print("OECD averages:")
-print(total_rates[2000])
-
-labels = [""] * (end_year - rate_data_starting_year - 2)
-labels.append("OECD average" + "<br>(rate)")
-
-fig.add_trace(go.Scatter(
-    x=x_data,
-    y=y_data,
-    mode="lines+text",  # no markers
-    name='OECD average',
-    legendgroup='OECD average',
-    text=labels,
-    textposition="top center",
-    showlegend=True,
-    visible='legendonly',
-    line=dict(color=colours[line_colour],dash='dash')
-),
-    secondary_y=True)
 
 
 # make legend more cramped, so all countries fit
